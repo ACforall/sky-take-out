df4a455009eb79ce303bd18ce93e42b4acdd7303
jessie <2445392384@qq.com>
2026-01-15 17:01:40 +0800

    订单状态定时处理代码开发

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,63 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.formula.functions.T;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Component
+@Slf4j
+/**
+ * 定时处理订单状态
+ */
+public class OrderTask {
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 每分钟处理超时订单
+     */
+    @Scheduled(cron = "0 * * * * *")
+//    @Scheduled(cron = "0/5 * * * * ?")
+    public void processTimeoutOrder(){
+        log.info("定时处理超时订单：{}", LocalDateTime.now());
+
+        LocalDateTime time = LocalDateTime.now().plusMinutes(-15);
+
+        //select * from orders where status =? and order_time<=(当前时间-15min）
+        List<Orders> orderList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, time);
+
+        if(orderList!=null && !orderList.isEmpty()){
+            for(Orders orders:orderList){
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelReason("订单超时，自动取消");
+                orders.setCancelTime(LocalDateTime.now());
+                orderMapper.update(orders);
+            }
+        }
+    }
+
+    /**
+     * 处理每天一直处于派送中状态的订单
+     */
+    @Scheduled(cron = "0 0 1 * * *")
+//    @Scheduled(cron = "2/5 * * * * ?")
+    public void processDeliveryOrder(){
+        log.info("定时处理处理派送中的订单：{}", LocalDateTime.now());
+
+        LocalDateTime time=LocalDateTime.now().plusMinutes(-60);
+        List<Orders> orderList=orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, time);
+        if(orderList!=null && !orderList.isEmpty()){
+            for(Orders orders:orderList){
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
